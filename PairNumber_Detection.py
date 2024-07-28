c10657987ef524c3e7d1824d250ca5b55c11338d
MPM3KOR <manjularparmar@gmail.com>
2024-07-28 11:25:45 +0530

    Update PairNumber_Detection.py

--- PairNumber_Detection.py
+++ PairNumber_Detection.py
@@ -1,15 +1,15 @@
-from Color_Code_main import color_pair_to_string
+from Color_Code_main
 from Exceptions_Messages import Exception_major_index_OutOfRange , Exception_minor_index_OutOfRange
 
 def get_pair_number_from_color(major_color, minor_color):
   try:
-    major_index = MAJOR_COLORS.index(major_color)
+    major_index = Color_Code_main.MAJOR_COLORS.index(major_color)
   except ValueError:
     Exception_major_index_OutOfRange()
   
   try:
-    minor_index = MINOR_COLORS.index(minor_color)
+    minor_index = Color_Code_main.MINOR_COLORS.index(minor_color)
   except ValueError:
     Exception_minor_index_OutOfRange()
     
-  return major_index * len(MINOR_COLORS) + minor_index + 1
+  return major_index * len(Color_Code_main.MINOR_COLORS) + minor_index + 1
