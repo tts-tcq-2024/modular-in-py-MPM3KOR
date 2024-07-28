9276b4f4275fe3e70e879c9b49708e85ad561e37
MPM3KOR <manjularparmar@gmail.com>
2024-07-28 13:29:07 +0530

    Update PairNumber_Detection_Test.py

--- PairNumber_Detection_Test.py
+++ PairNumber_Detection_Test.py
@@ -1,6 +1,6 @@
-from PairNumber_Detection import get_pair_number_from_color
+import PairNumber_Detection
 
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
-  pair_number = get_pair_number_from_color(major_color, minor_color)
+  pair_number = PairNumber_Detection.get_pair_number_from_color(major_color, minor_color)
   
   assert(pair_number == expected_pair_number)
