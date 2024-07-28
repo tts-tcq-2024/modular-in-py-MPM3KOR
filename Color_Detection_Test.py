632a94c299fc1d5fe804a23e4eead8be3522c2b2
MPM3KOR <manjularparmar@gmail.com>
2024-07-28 10:18:45 +0530

    Create Color_Detection_Test.py

---
+++ Color_Detection_Test.py
@@ -0,0 +1,9 @@
+from Color_Detection import *
+
+def test_number_to_pair(pair_number,expected_major_color, expected_minor_color):
+  
+  major_color, minor_color = get_color_from_pair_number(pair_number)
+  
+  assert(major_color == expected_major_color)
+  assert(minor_color == expected_minor_color)
+  
