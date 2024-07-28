0ec63e849b4b7729d65d2dd2becb98521cf6ae66
MPM3KOR <manjularparmar@gmail.com>
2024-07-28 10:13:01 +0530

    Create PairNumber_Detection.py

---
+++ PairNumber_Detection.py
@@ -0,0 +1,14 @@
+from main import *
+
+def get_pair_number_from_color(major_color, minor_color):
+  try:
+    major_index = MAJOR_COLORS.index(major_color)
+  except ValueError:
+    Exception_major_index_OutOfRange()
+  
+  try:
+    minor_index = MINOR_COLORS.index(minor_color)
+  except ValueError:
+    Exception_minor_index_OutOfRange()
+    
+  return major_index * len(MINOR_COLORS) + minor_index + 1
