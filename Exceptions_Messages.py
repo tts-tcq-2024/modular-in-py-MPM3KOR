65795b8de8e4e86457b4b4164daf00181c8c0c02
MPM3KOR <manjularparmar@gmail.com>
2024-07-28 10:14:26 +0530

    Create Exceptions_Messages.py

---
+++ Exceptions_Messages.py
@@ -0,0 +1,9 @@
+from main import *
+
+
+
+def Exception_major_index_OutOfRange():
+    raise Exception('Major index out of range')
+
+def Exception_minor_index_OutOfRange():
+    raise Exception('Minor index out of range')
