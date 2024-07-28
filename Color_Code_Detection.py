4fd21be7b1ce26250066a5d09c782dd2e60a3947
MPM3KOR <manjularparmar@gmail.com>
2024-07-28 11:22:48 +0530

    Update Color_Code_Detection.py

--- Color_Code_Detection.py
+++ Color_Code_Detection.py
@@ -1,15 +1,15 @@
-from Color_Code_main import color_pair_to_string
+from Color_Code_main
 from Exceptions_Messages import Exception_major_index_OutOfRange , Exception_minor_index_OutOfRange
 
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
   
   major_index = zero_based_pair_number // len(MINOR_COLORS)
-  if major_index >= len(MAJOR_COLORS):
+  if major_index >= len(Color_Code_main.MAJOR_COLORS):
     Exception_major_index_OutOfRange()
   
-  minor_index = zero_based_pair_number % len(MINOR_COLORS)
-  if minor_index >= len(MINOR_COLORS):
+  minor_index = zero_based_pair_number % len(Color_Code_main.MINOR_COLORS)
+  if minor_index >= len(Color_Code_main.MINOR_COLORS):
     Exception_minor_index_OutOfRange()
  
-  return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
+  return Color_Code_main.MAJOR_COLORS[major_index], Color_Code_main.MINOR_COLORS[minor_index]
